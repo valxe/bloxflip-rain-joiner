02650564b2c474e1e4395957a6a80db162130f4a
Valentine <111288675+valxe@users.noreply.github.com>
2024-04-24 22:05:07 +0200

    Update bloxflip.user.js
    Removed useless stuff cuz im bored and also added stuff cuz im bored

--- bloxflip.user.js
+++ bloxflip.user.js
@@ -2,7 +2,7 @@
 // @name         Bloxflip Auto Rain
 // @namespace    http://tampermonkey.net/
 // @version      1.0.0
-// @description  Notifies about rain and join i guesssss
+// @description  Auto join rain i guesss
 // @author       Valentineuh
 // @match        https://bloxflip.com/*
 // @icon         https://bloxflip.com/favicon.ico
@@ -11,28 +11,33 @@
 // ==/UserScript==
 
 window.addEventListener('load', () => {
-    console.log(Notification.permission);
-    if (Notification.permission !== "granted" && Notification.permission !== "denied") {
-        alert("Enable notifications for Bloxflip to get rain alerts.");
-        Notification.requestPermission().then(console.log);
-    }
-
-    const notify = (title, msg) => {
-        new Notification(title, { body: msg });
-    };
+    console.log("Script loaded.");
 
     let raining = false;
 
     setInterval(async () => {
         let data = await (await fetch('https://api.bloxflip.com/chat/history')).json();
         if (data.rain.active && !raining) {
-            notify("Bloxflip Rain", "A rain event is happening now!");
+            console.log("Rain detected!");
             raining = true;
             setTimeout(() => {
                 document.querySelector(".chat_chatBannerJoinButton__avNuN").click();
             }, 5000);
+
         } else if (!data.rain.active && raining) {
+            console.log("Rain event ended.");
             raining = false;
         }
     }, 5000);
+
+    setInterval(async () => {
+        console.log("Checking balance...");
+        const userBalance = document.querySelector('.header_headerUserBalance__mNiaf');
+        if (userBalance) {
+            const balance = userBalance.querySelector('span').textContent;
+            console.log("Balance:", balance);
+        } else {
+            console.log("User balance element not found.");
+        }
+    }, 600000);
 });
